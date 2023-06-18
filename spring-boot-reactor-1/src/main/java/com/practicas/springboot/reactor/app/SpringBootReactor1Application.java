678cdffe7e8d1b2fc424b2f73c9785a300de5ac6
alexzgp <102426904+alexzgp@users.noreply.github.com>
2023-06-18 12:01:20 +0200

    Creando proyecto con Reactor API

--- spring-boot-reactor-1/src/main/java/com/practicas/springboot/reactor/app/SpringBootReactor1Application.java
+++ spring-boot-reactor-1/src/main/java/com/practicas/springboot/reactor/app/SpringBootReactor1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactor1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootReactorApplication.class, args);
+		SpringApplication.run(SpringBootReactor1Application.class, args);
 	}
 
 }
