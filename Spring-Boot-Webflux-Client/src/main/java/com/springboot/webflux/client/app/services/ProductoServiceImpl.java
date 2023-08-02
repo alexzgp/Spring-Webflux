6be2ac10e40d785917c5ad71c5651cc62f3a8a6e
alexzgp <102426904+alexzgp@users.noreply.github.com>
2023-08-02 16:18:44 +0200

    añadimos las funcionalidades del servicio

--- Spring-Boot-Webflux-Client/src/main/java/com/springboot/webflux/client/app/services/ProductoServiceImpl.java
+++ Spring-Boot-Webflux-Client/src/main/java/com/springboot/webflux/client/app/services/ProductoServiceImpl.java
@@ -3,13 +3,19 @@ package com.springboot.webflux.client.app.services;
 import com.springboot.webflux.client.app.models.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class ProductoServiceImpl implements ProductoService{
 
     @Autowired
@@ -36,16 +42,27 @@ public class ProductoServiceImpl implements ProductoService{
 
     @Override
     public Mono<Producto> save(Producto producto) {
-        return null;
+        return client.post()
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(producto))
+                .exchangeToMono(response -> response.bodyToMono(Producto.class));
     }
 
     @Override
     public Mono<Producto> update(Producto producto, String id) {
-        return null;
+        return client.put()
+                .uri("/{id}", Collections.singletonMap("id", id))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(producto))
+                .exchangeToMono(response -> response.bodyToMono(Producto.class));
     }
 
     @Override
     public Mono<Void> delete(String id) {
-        return null;
+        return client.delete()
+                .uri("/{id}", Collections.singletonMap("id", id))
+                .exchangeToMono(ClientResponse::releaseBody);
     }
 }
