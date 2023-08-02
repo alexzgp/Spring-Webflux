cc1135431c5518cb10e1e653f2f816b9abcdf017
alexzgp <102426904+alexzgp@users.noreply.github.com>
2023-08-02 15:40:09 +0200

    creamos la base de un nuevo proyecto WebClient

--- Spring-Webflux-ApiRest/src/test/java/com/spring/webflux/apirest/app/SpringWebfluxApiRestApplicationTests.java
+++ Spring-Webflux-ApiRest/src/test/java/com/spring/webflux/apirest/app/SpringWebfluxApiRestApplicationTests.java
@@ -1,13 +1,27 @@
 package com.spring.webflux.apirest.app;
 
+import com.spring.webflux.apirest.app.models.documents.Producto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringWebfluxApiRestApplicationTests {
 
+	@Autowired
+	private WebTestClient client;
 	@Test
-	void contextLoads() {
+	private void listarTest() {
+		client.get()
+				.uri("/api/v2/productos")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBodyList(Producto.class);
 	}
 
 }
